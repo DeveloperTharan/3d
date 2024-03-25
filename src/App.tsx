b8e4923b2da5f057d30329a924553f67f1320806
Dharanitharan <hhshjjahgy@gmail.com>
2024-03-25 21:16:46 +0000

    ./

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import { gsap } from "gsap";
-
 export default function App() {
-  const a = gsap.version;
-  console.log(a);
-
-  return (
-    <div className="w-full h-full flex items-center justify-center text-3xl font-bold text-green-600">
-      3d
-    </div>
-  );
+  return <></>;
 }
